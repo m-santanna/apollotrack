53fe7a9953d9b7eee002f04a7f2bc5c0f0d7f8ee
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-03-26 23:55:22 +0000

    actions updated to handle new formdata and table

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,7 +4,7 @@ import { db } from '@/src/db'
 import { auth } from '@/lib/auth'
 import { revalidatePath } from 'next/cache'
 import { headers } from 'next/headers'
-import { user_macros, food_item } from '@/src/db/schema'
+import { user_macros, user_food } from '@/src/db/schema'
 import { calculateCalories, calculateMacros } from '@/lib/macros-utils'
 import { eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
@@ -117,28 +117,20 @@ export async function addFoodItems(formData: FormData) {
     const userId = session.user.id
 
     const names = formData.getAll('name')
-    const calories = formData.getAll('calories')
-    const proteins = formData.getAll('protein')
-    const carbs = formData.getAll('carbs')
-    const fats = formData.getAll('fat')
+    const foodIds = formData.getAll('foodId')
     const total_grams = formData.getAll('total_grams')
     const prices = formData.getAll('price')
-    const categories = formData.getAll('category')
 
     const items = names.map((_, index) => ({
         userId,
+        foodId: foodIds[index] as string,
         name: names[index] as string,
-        calories: Number(calories[index]),
-        protein: Number(proteins[index]),
-        carbs: Number(carbs[index]),
-        fat: Number(fats[index]),
         total_grams: Number(total_grams[index]),
         price: Number(prices[index]),
-        category: categories[index] as string,
     }))
 
     items.map(async (item) => {
-        await db.insert(food_item).values(item)
+        await db.insert(user_food).values(item)
     })
 
     revalidatePath('/dashboard')
