640da46150cf4713203f5806abf50c1a0b6ee6c4
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-05-20 04:34:26 +0100

    switch to vercel

--- app.config.ts
+++ app.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from '@tanstack/react-start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  server: {
-    preset: 'netlify',
-  },
-  vite: {
-    plugins: [
-      tsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-    ],
-  },
+    server: {
+        preset: 'vercel',
+    },
+    vite: {
+        plugins: [
+            tsConfigPaths({
+                projects: ['./tsconfig.json'],
+            }),
+        ],
+    },
 })
